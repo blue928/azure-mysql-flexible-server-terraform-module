2e69d5b0b1e46670e06be160f7c5e60a3f983c6a
Blue Presley <bluepresley@gastonia.com>
2022-03-09 12:27:06 -0500

    update server config resources names to remove duplicates

--- mysql-flexible-server-parameters.tf
+++ mysql-flexible-server-parameters.tf
@@ -7,28 +7,28 @@
 # - - Find the mysql flexible server in question
 # - - click "Server Parameters" on the left. 
 
-resource "azurerm_mysql_flexible_server_configuration" "fs_db" {
+resource "azurerm_mysql_flexible_server_configuration" "require_secure_transport" {
   name                = "require_secure_transport"
   resource_group_name = var.resource_group_name
   server_name         = azurerm_mysql_flexible_server.fs_db.name
   value               = "OFF"
 }
 
-resource "azurerm_mysql_flexible_server_configuration" "fs_db" {
+resource "azurerm_mysql_flexible_server_configuration" "audit_log_enabled" {
   name                = "audit_log_enabled"
   resource_group_name = var.resource_group_name
   server_name         = azurerm_mysql_flexible_server.fs_db.name
   value               = "ON"
 }
 
-resource "azurerm_mysql_flexible_server_configuration" "fs_db" {
+resource "azurerm_mysql_flexible_server_configuration" "audit_log_events" {
   name                = "audit_log_events"
   resource_group_name = var.resource_group_name
   server_name         = azurerm_mysql_flexible_server.fs_db.name
   value               = "ADMIN,CONNECTION,DCL,DDL,DML,DML_NONSELECT,DML_SELECT,GENERAL,TABLE_ACCESS"
 }
 
-resource "azurerm_mysql_flexible_server_configuration" "fs_db" {
+resource "azurerm_mysql_flexible_server_configuration" "slow_query_log" {
   name                = "slow_query_log"
   resource_group_name = var.resource_group_name
   server_name         = azurerm_mysql_flexible_server.fs_db.name
